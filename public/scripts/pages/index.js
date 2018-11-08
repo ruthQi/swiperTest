fbce73ecb6151ea4ab754b8a932822928ef0dd04
qiru <bjqiru@corp.netease.com>
2018-11-08 18:23:47 +0800

    swiper测试

--- public/scripts/pages/index.js
+++ public/scripts/pages/index.js
@@ -1,5 +1,4 @@
-export default function() {
-
-    console.log('index page!!!');
-
-};
+var Swiper = require('libs/swiper-3.4.min');//布局需为flex布局
+new Swiper('.swiper-container', {
+   direction: 'horizontal'
+})
